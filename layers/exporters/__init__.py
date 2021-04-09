f33be07eb525b587d577120ec8401098d6354fb1
Isabel Tuson <isabel@mitre.org>
2021-04-09 13:51:16 -0400

    updated documentation and removed layerlib to reflect migration to mitreattack-python

--- layers/exporters/__init__.py
+++
@@ -1,5 +0,0 @@
-from .excel_templates import ExcelTemplates
-from .matrix_gen import MatrixGen
-from .to_excel import ToExcel
-from .to_svg import ToSvg, SVGConfig
-from .svg_templates import SvgTemplates
